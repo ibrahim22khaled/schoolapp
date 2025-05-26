5d7fa37e1a20b9383f1c68ffb63d9f82e5aecd2e
mohamedabdelaal11 <abdelaal@moltaqa.net>
2025-05-26 15:40:17 +0300

    home

--- lib/features/dashboard/dashboard.dart
+++ lib/features/dashboard/dashboard.dart
@@ -9,39 +9,36 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-        Stack(
-          children:[
-            Positioned(
-          top: 204,
-          left: 16,
-          child: CustomContainer(
-            image: "assets/images/attendence.png",
-            title: "80.39%",
-            subtitle: "Attendance",
-          ),
+      body: Stack(alignment: Alignment.center, children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.topLeft,
+                  colors: [Color(KfirstColor), Color(Ksecondcolor)])),
+          child: DashboardDetails(),
         ),
         Positioned(
-          top: 204,
-          left: 196,
-          child: CustomContainer(
-            image: "assets/images/fees.png",
-            title: "\$6400",
-            subtitle: "Fees Due",
+          top: MediaQuery.of(context).size.height * 0.27,
+          child: Row(
+            children: [
+              CustomContainer(
+                image: "assets/images/attendence.png",
+                title: "80.39%",
+                subtitle: "Attendance",
+              ),
+              const SizedBox(width: 10),
+              CustomContainer(
+                image: "assets/images/fees.png",
+                title: "\$6400",
+                subtitle: "Fees Due",
+              ),
+            ],
           ),
         ),
-             Container(
-            width: double.infinity,
-            height: double.infinity,
-             decoration: BoxDecoration(
-            gradient: LinearGradient(
-             begin:  Alignment.topCenter,
-             end: Alignment.topLeft,
-              colors: [Color(KfirstColor), Color(Ksecondcolor)])
-          ),
-            child: DashboardDetails(),
-            ),
-       ] ),
+      ]),
     );
   }
 }
--- lib/features/dashboard/widgets/custom_container.dart
+++ lib/features/dashboard/widgets/custom_container.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CustomContainer extends StatelessWidget {
-  final image ;
-  final title ;
-  final subtitle; 
-  const CustomContainer({super.key,  required this.image,required  this.title, required this.subtitle});
+  final image;
+  final title;
+  final subtitle;
+  const CustomContainer(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.subtitle});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 202,
-      width: 163,
+      // height: 202,
+      width: MediaQuery.of(context).size.width / 2 - 20,
+
       decoration: BoxDecoration(
         border: Border.all(color: Color(0xff5278C1)),
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
       ),
       child: Padding(
-        padding: const EdgeInsets.only(left: 19.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           children: [
             const SizedBox(height: 15),
             Image.asset(image, width: 72, height: 72),
